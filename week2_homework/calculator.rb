4d611dcbc49f3b5386b0ac75408356253fd80293
Garrett Sand <garrettsand@gmail.com>
2014-04-15 23:35:17 -0700

    Fixed whitespace

--- week2_homework/calculator.rb
+++ week2_homework/calculator.rb
@@ -1,36 +1,36 @@
 class Calculator
-	def sum(content)
-		total = 0
+  def sum(content)
+    total = 0
 
-		content.each do |number|
-			total += number 
-		end
+    content.each do |number|
+      total += number 
+    end
 
-		total 
-	end
+    total 
+  end
 
-	def multiply(x, y = nil)
-		if x.is_a?(Array)
-			x[0] * x[1]
-		else
-			x * y
-		end
-	end
+  def multiply(x, y = nil)
+    if x.is_a?(Array)
+      x[0] * x[1]
+    else
+      x * y
+    end
+  end
 
-	def multiply(*numbers)
-		numbers = numbers.flatten
-		numbers[0] * numbers[1]
-	end
+  def multiply(*numbers)
+    numbers = numbers.flatten
+    numbers[0] * numbers[1]
+  end
 
-	def pow(x, y)
-		x ** y
-	end
+  def pow(x, y)
+    x ** y
+  end
 
-	def fac(n)
-		if n == 0
-			1
-		else
-			n * fac(n - 1)
-		end
-	end
+  def fac(n)
+    if n == 0
+      1
+    else
+      n * fac(n - 1)
+    end
+  end
 end
